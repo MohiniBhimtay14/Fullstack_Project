0b9e5a347482be0d9102c8ce5a5cfd0910360704
Mohini Bhimtay <mohinibhimtay1289@gmail.com>
2025-04-10 01:42:31 +0530

    sign-up schema done

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -1,5 +1,69 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/user";
-import {z} from "zod";
+import { z } from "zod";
+import { usernameValidation } from "@/schemas/signUpSchema";
 
+const UsernameQuerySchema = z.object({
+  username: usernameValidation,
+});
 
+export async function GET(request: Request) {
+
+  await dbConnect();
+  //   localhost:3000/api/check-username-unique?username=mohini
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const queryParam = {
+      username: searchParams.get("username"),
+    };
+    // Validate with zod
+    const result = UsernameQuerySchema.safeParse(queryParam);
+    console.log("result", result); // TODO: remove this line
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+      return Response.json(
+        {
+          success: false,
+          message:
+            usernameErrors?.length > 0
+              ? usernameErrors.join(", ")
+              : "Invalid username",
+        },
+        { status: 400 }
+      );
+    }
+    const { username } = result.data;
+
+    const existingVerifiedUser = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+
+    if (existingVerifiedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        { status: 400 }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: "Username is unique",
+      },
+      { status: 400 }
+    );
+  } catch (error) {
+    console.error("Error checking username uniqueness:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
+  }
+}
